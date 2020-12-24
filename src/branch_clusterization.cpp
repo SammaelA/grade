8e59a753903126274e15d7a9be6546fc68db6a18
Garifullin Albert <albgar-14@yandex.ru>
2020-12-24 14:19:54 +0300

    clusterization WIP 1

--- src/branch_clusterization.cpp
+++ src/branch_clusterization.cpp
@@ -75,29 +75,46 @@ bool dedicated_bbox(Branch *branch, BillboardCloud::BBox &bbox)
     bbox = BillboardCloud::get_bbox(branch,a,b,c);
     return true;
 }
-Clusterizer::Answer Clusterizer::dist(Branch *b1, Branch *b2, float min, float max)
+float partial_dist(std::vector<int> &jc, std::vector<float> &matches,  std::vector<float> &weights)
 {
-    if (!b1 || !b2)
+    float num=0.0;
+    float denom = 0.0;
+    for (int i=0;i<matches.size();i++)
     {
-        return Answer(true,0,0);
+        num += weights[i]*(2*matches[i]);
+        denom +=weights[i]*jc[i];
     }
+    return denom > 0.001 ? 1 - num/denom : 0;
+}
+bool Clusterizer::match_child_branches(Joint *j1, Joint *j2, std::vector<float> &matches, std::vector<int> &jc, float min, float max)
+{
+    int sz1 = j1->childBranches.size();
+    int sz2 = j2->childBranches.size();
+    if (sz1 == 0 || sz2 == 0)
+        return true;
+    else if (sz1 == 1 && sz2 == 1)
+    {
+        return match_joints(j1->childBranches.front(), j2->childBranches.front(), matches, jc, min, max);
+    }
+    else if (sz1 == 1 || sz2 == 1)
+    {
+        if (sz2 == 1)
+        {
+            Joint *tmp = j1;
+            j1 = j2;
+            j2 = tmp;
+        }
+
+    }
+}
+bool Clusterizer::match_joints(Branch *b1, Branch *b2, std::vector<float> &matches, std::vector<int> &jc, float min, float max)
+{
     if (b1->joints.size() == 1)
     {
         if (b2->joints.size() == 1)
-            return Answer(true,0,0);
-        else
-            return Answer(true,1,1);
-        
+            matches[b1->level]++;
+        return true;
     }
-    /*BillboardCloud::BBox bbox1, bbox2;
-    if (dedicated_bbox(b1,bbox1) && dedicated_bbox(b2,bbox2)) 
-    {
-
-    }
-    else
-    {
-        return Answer(true,0,0);//all broken branches are similar to each other
-    }*/
     float av_len = 0.5*(length(b1->joints.back().pos - b1->joints.front().pos) + length(b2->joints.back().pos - b2->joints.front().pos));
     float cur_delta = delta*av_len;
     float cur_dist = 0;
@@ -115,8 +132,6 @@ Clusterizer::Answer Clusterizer::dist(Branch *b1, Branch *b2, float min, float m
             {
                 distances.emplace(JSortData(len, &j1, &j2));
             }
-            
-
         }
     }
     auto it = distances.begin();
@@ -135,104 +150,49 @@ Clusterizer::Answer Clusterizer::dist(Branch *b1, Branch *b2, float min, float m
         }
     }
     //after it only correct matches remained here
-    int matches = matched_joints.size();
-    cur_dist = 0.5*(b1->joints.size() + b2->joints.size() - matches)/(float)(b1->joints.size() + b2->joints.size());
-    Answer part_answer(true,cur_dist,1 - (0.5-cur_dist));
-    float subbranches_j_count = b1->base_seg_n + b2->base_seg_n - b1->joints.size() - b2->joints.size();
-    subbranches_j_count *= 2.0;
-    int matched_j = 0;
-    if (subbranches_j_count < 1)
-    {
-        #if DEBUG
-            fprintf(stderr," fast dist %f exact = 1",2*cur_dist);
-        #endif
-        return Answer(true,2*cur_dist,2*cur_dist);
-    }
+    int matches_count = distances.size();
+    matches[b1->level] += matches_count;
+    if (partial_dist(jc,matches,weights) > min)
+        return false;
+    if (b1->level == matches.size() - 1)//this is the last branch level, no need to iterate over child branches
+        return true;
     it = distances.begin();
     while (it != distances.end())
     {
-        if (part_answer.from > min || part_answer.to < max)
+        if (!match_child_branches(it->j1,it->j2,matches,jc,min,max))
         {
-            part_answer.exact = false;
-            break;
+            //if child branches matching made early exit, it means that min distance limit
+            //is already passed. No need to go further.
+            return false;
         }
-        Answer min_d = Answer(false,1,-1);
-        int min_sz = 0;
-        int sz1 = it->j1->childBranches.size();
-        int sz2 = it->j2->childBranches.size();
-        if (sz1 == 0 || sz2 == 0)
-        {
-            
-            if (sz1)
-            {
-                for (Branch *b1 : it->j1->childBranches)
-                {
-                    matched_j += b1->base_seg_n;
-                    part_answer.from += b1->base_seg_n/subbranches_j_count;
-                }
-            }   
-            else if (sz2)
-            {
-                for (Branch *b2 : it->j2->childBranches)
-                {
-                    matched_j += b2->base_seg_n;
-                    part_answer.from += b2->base_seg_n/subbranches_j_count;
-                }
-            }
-            it++;
-            continue;
-        }
-        else if (sz1 == 1)
-        {
-            Branch *f = it->j1->childBranches.front();
-            for (Branch *f2 : it->j2->childBranches)
-            {
-                Answer a = dist(f,f2,(min - cur_dist)*subbranches_j_count/(f->base_seg_n + f2->base_seg_n),
-                                (max - cur_dist)*subbranches_j_count/(f->base_seg_n + f2->base_seg_n));
-                if (a.from<min_d.from)
-                {
-                    min_d = a;
-                    min_sz = f->base_seg_n + f2->base_seg_n;
-                }
-            }
-            float k = min_sz/subbranches_j_count;
-            part_answer.from += k*min_d.from;
-            part_answer.to -= k*(1 - min_d.to);
-        }  
-        else if (sz2 == 1)
-        {
-            Branch *f = it->j2->childBranches.front();
-            for (Branch *f2 : it->j1->childBranches)
-            {
-                Answer a = dist(f,f2,(min - cur_dist)*subbranches_j_count/(f->base_seg_n + f2->base_seg_n),
-                                (max - cur_dist)*subbranches_j_count/(f->base_seg_n + f2->base_seg_n));
-                if (a.from<min_d.from)
-                {
-                    min_d = a;
-                    min_sz = f->base_seg_n + f2->base_seg_n;
-                }
-            }
-            float k = min_sz/subbranches_j_count;
-            part_answer.from += k*min_d.from;
-            part_answer.to -= k*(1 - min_d.to);
-        }
-        else
-        {
-            it++;
-            continue;
-        }
-        matched_j += min_sz;
         it++;
     }
-    float unmatched_joints = (0.5*subbranches_j_count - matched_j)/subbranches_j_count;
-    part_answer.from += unmatched_joints;
+    return true;
+
+}
+Clusterizer::Answer Clusterizer::dist(BranchWithData &bwd1, BranchWithData &bwd2, float min, float max)
+{
+    Branch *b1 = bwd1.b;
+    Branch *b2 = bwd2.b;
+    Answer part_answer;
+    std::vector<int> joint_counts(bwd1.joint_counts);
+    std::vector<float> matches(joint_counts.size());
+        fprintf(stderr,"size = %d joints count by level [", matches.size());
+    for (int i=0;i<joint_counts.size();i++)
+    {
+        joint_counts[i] += bwd2.joint_counts[i];
+        fprintf(stderr,"%d ",joint_counts[i]);
+    }
+        fprintf(stderr,"]\n");
+    part_answer.exact = match_joints(b1,b2,matches,joint_counts,min,max);
+    part_answer.from = partial_dist(joint_counts,matches,weights);
+    part_answer.to = part_answer.exact ? part_answer.from : 1;
     #if DEBUG
     fprintf(stderr," level = %d size = %d dist %f/1 %d/ (%d + %d) matches\n",b1->level,b1->base_seg_n,cur_dist, distances.size(),b1->joints.size() ,
      b2->joints.size());
     #endif
-    #if DEBUG
-        fprintf(stderr," full dist [%f %f] exact = %d\n",part_answer.from, part_answer.to, part_answer.exact);
-    #endif
+    
+    fprintf(stderr," full dist [%f %f] exact = %d\n",part_answer.from, part_answer.to, part_answer.exact);
     
     return part_answer;
 }
@@ -247,6 +207,7 @@ bool Clusterizer::set_branches(Tree &t, int layer)
     }
     else
     {
+        int i = 0;
         for (Branch &b : t.branchHeaps[layer]->branches)
         {
             //if (b.dead)
@@ -266,9 +227,12 @@ bool Clusterizer::set_branches(Tree &t, int layer)
                 mat4 SC_inv = inverse(SC);
                 rot = SC_inv*transl*rot;
                 transform_branch(nb,rot);
+                branches.push_back(BranchWithData(nb,t.params.max_depth(),i));
+                i++;
                 
             }
         }
+        ddt.create(i);
         if (branchHeap.branches.size() > 2)
         {
             int i = 0;
@@ -280,25 +244,22 @@ bool Clusterizer::set_branches(Tree &t, int layer)
                     gistos[j1][j2] = 0;
                 }
             }
-            for (Branch &b : branchHeap.branches)
+            for (BranchWithData &b : branches)
             {
-                if (b.level != layer)
-                    continue;
-                for (Branch &b2 : branchHeap.branches)
+                for (BranchWithData &b2 : branches)
                 {
-                    if (b.level ==  b2.level)
-                    {
+                    if (b.b == b2.b)
+                        continue;
                         fprintf(stderr,"r(%d, %d) = ",0,i);
                         for (int k=1;k<20;k++)
                         {
                             delta = 0.05*k;
-                            Answer a = dist(&(b),&(b2));
+                            Answer a = dist(b,b2);
                             gistos[k][(int)(10*a.from)] ++;
                             fprintf(stderr,"%f ", a.from);
                         }
                         i++;
                         fprintf(stderr,"\n");
-                    }
                 }
             }
             fprintf(stderr,"gistogramms\n");
@@ -320,4 +281,40 @@ bool Clusterizer::set_branches(Tree &t, int layer)
         
     }
     
+}
+void Clusterizer::calc_joints_count(Branch *b, std::vector<int> &counts)
+{
+    if (b->dead)
+        return;
+    for (Joint &j : b->joints)
+    {
+        counts[b->level]++;
+        for (Branch *br : j.childBranches)
+        {
+            calc_joints_count(br,counts);
+        }
+    }
+}
+Clusterizer::Answer Clusterizer::cluster_dist_min(Cluster &c1, Cluster &c2, float min = 1.0, float max = 0.0)
+{
+    float cur_min = 1.0;
+    for (BranchWithData &bwd1 : c1.branches)
+    {
+        for (BranchWithData &bwd2 : c2.branches)
+        {
+            Answer a = ddt.get(bwd1.pos,bwd2.pos);
+            if (a.from > cur_min)
+                continue;
+            else if (a.exact)
+                cur_min = a.from;
+            else
+            {
+                a = dist(bwd1, bwd2, cur_min, 0.0f);
+                ddt.set(bwd1.pos,bwd2.pos,a);
+                if (a.from<cur_min)
+                    cur_min = a.from;
+            }
+            
+        }
+    }
 }
--- src/branch_clusterization.h
+++ src/branch_clusterization.h
@@ -35,12 +35,35 @@ class Clusterizer
         int size() {return n;}
         ~DistDataTable() {clear();}
     };
+    static void calc_joints_count(Branch *b, std::vector<int>  &counts);
+    struct BranchWithData
+    {
+        Branch *b;
+        int pos;
+        std::vector<int> joint_counts;
+        BranchWithData(Branch *_b = nullptr, int levels = 0, int pos = 0)
+        {
+            b = _b;
+            for (int i=0;i<levels;i++)
+                joint_counts.push_back(0);
+            if (b)
+                calc_joints_count(b,joint_counts);
+        }
+    };
     struct Cluster
     {
-        std::vector<Branch *> branches;
+        std::vector<BranchWithData> &branches;
     };
+    struct 
     bool set_branches(Tree &t, int layer);
-    Answer dist(Branch *b1, Branch *b2, float min = 1.0, float max = 0.0);
+    bool match_joints(Branch *b1, Branch *b2, std::vector<float> &matches, std::vector<int> &jc, float min, float max);
+    bool match_child_branches(Joint *j1, Joint *j2, std::vector<float> &matches, std::vector<int> &jc, float min, float max);
+    Answer dist(BranchWithData &bwd1, BranchWithData &bwd2, float min = 1.0, float max = 0.0);
+    Answer cluster_dist_min(Cluster &c1, Cluster &c2, float min = 1.0, float max = 0.0);
+    void 
     float delta = 0.5;
     BranchHeap branchHeap;
+    std::vector<BranchWithData> branches;
+    DistDataTable ddt;
+    std::vector<float> weights{0.6, 0.25, 0.1, 0.035, 0.015};
 }; 
