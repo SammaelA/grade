2f150a5b5ba73fcebd80f9c6a5c3385a1c835781
sammael <albgar-14@yandex.ru>
2021-08-02 15:43:42 +0300

    CT-9 Polycentric simulated annealing

--- src/parameter_selection.cpp
+++ src/parameter_selection.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <functional>
 #include <chrono>
+#include <set>
 
 float brute_force_selection(TreeStructureParameters &param, Metric *metric,
                             std::function<void(TreeStructureParameters &, GrovePacked &)> &generate)
@@ -229,12 +230,18 @@ float brute_force_selection(TreeStructureParameters &param, Metric *metric,
     return max_metr;
 }
 float simulated_annealing_selection(TreeStructureParameters &param, Metric *metric,
-                                    std::function<void(TreeStructureParameters &, GrovePacked &)> &generate)
+                                    std::function<void(TreeStructureParameters &, GrovePacked &)> &generate,
+                                    int *iterations = nullptr,
+                                    double _alpha = 0.9,
+                                    double _min_metric = 0.1,
+                                    double val_interval = 0.5,
+                                    double pos_change_chance_min = 0.05,
+                                    double pos_change_chance_max = 0.25)
 {
     double first_run, second_run, third_run;        //(first, second and third run) are defined for the purpose of comparing the resulting
-    const double  alpha = 0.995;                         //alpha is used for the cooling schedule of the temperature            
+    const double  alpha = _alpha;                         //alpha is used for the cooling schedule of the temperature            
     const double e = 2.718281828;
-    const double min_metric = 0.1;
+    const double min_metric = _min_metric;
 
     std::vector<ParameterDesc> mask;
     std::vector<double> data;
@@ -256,7 +263,7 @@ float simulated_annealing_selection(TreeStructureParameters &param, Metric *metr
     data_max = data;
     double L = f(data);
     double max_L = L;
-    double max_T = 80;
+    double max_T = 100;
     int k = 0;
     bool *pos_changed = safe_new<bool>(data.size(),"pos_changed_arr");
     float *prev_val = safe_new<float>(data.size(),"prev_val_arr");
@@ -273,20 +280,20 @@ float simulated_annealing_selection(TreeStructureParameters &param, Metric *metr
             float val_changed = sqrt(T/max_T)*urand(-1,1);
             prev_val[och] = data[och];
             data[och] = CLAMP(data[och] + val_changed, 0, 1);
-            logerr("pos changed %d %f --> %f", och, prev_val[och], data[och]);
+            //logerr("pos changed %d %f --> %f", och, prev_val[och], data[och]);
         }
         else
         {
-            float pos_change_chance = CLAMP(sqrt(T/max_T), 0.025, 0.25);
+            float pos_change_chance = CLAMP(sqrt(T/max_T), pos_change_chance_min, pos_change_chance_max);
             for (int i=0;i<data.size();i++)
             {
                 if (urand()<pos_change_chance)
                 {
                     pos_changed[i] = true;
-                    float val_changed = sqrt(T/max_T)*urand(-1,1);
+                    float val_changed = val_interval*sqrt(T/max_T)*urand(-1,1);
                     prev_val[i] = data[i];
                     data[i] = CLAMP(data[i] + val_changed, 0, 1);
-                    logerr("pos changed %d %f --> %f", i, prev_val[i], data[i]);
+                    //logerr("pos changed %d %f --> %f", i, prev_val[i], data[i]);
                 }
                 else
                 {
@@ -320,9 +327,285 @@ float simulated_annealing_selection(TreeStructureParameters &param, Metric *metr
     safe_delete<bool>(pos_changed,"pos_changed_arr");
     safe_delete<float>(prev_val,"prev_val_arr");
     param.load_from_mask_and_data(mask, data_max);
-    logerr("base r %s",param.base_r.to_string().c_str());
-    logerr("r_split save power %s",param.r_split_save_pow.to_string().c_str());
+    if (iterations)
+        *iterations = k;
     return max_L;
+}
+
+float simulated_annealing_selection(TreeStructureParameters &param, Metric *metric,
+                            std::function<void(TreeStructureParameters &, GrovePacked &)> &generate,
+                            SetSelectionProgram &set_selection_program,
+                            ExitConditions exit_conditions)
+{
+    struct SACenter
+    {
+        int val_id;
+        float metr;
+        SACenter(int id, float met)
+        {
+            val_id = id;
+            metr = met;
+        }
+    };
+    struct compare
+    {
+        bool operator()(const SACenter &j1, const SACenter &j2) const
+        {
+            return j1.metr > j2.metr;
+        }
+    };
+
+    struct ParamTmp
+    {
+        int offset = 0;
+        int count = 0;
+        int data_pos = 0; //pos of modified parameter in data vector
+        std::vector<float> values;
+        std::string name = "unknown";
+    };
+
+    std::chrono::steady_clock::time_point t_start = std::chrono::steady_clock::now();
+    if (set_selection_program.selections.empty() || !metric)
+        return 0;
+    if (set_selection_program.schedule == AllInOne && set_selection_program.selections.size() > 1)
+    {
+        logerr("AllInOne schedule for multiple sets is not yet implemented");
+        set_selection_program.schedule = SetbySet;
+    }
+
+    std::vector<ParameterDesc> mask;
+    std::vector<double> data;
+    std::vector<double> data_max;
+    param.get_mask_and_data(mask, data);
+    data_max = data;
+    GrovePacked tree;
+    float max_metr = 0;
+    int global_iters = 0;
+    int global_centers = 0;
+    std::vector<SelectionSet> &selections = set_selection_program.selections;
+    if (set_selection_program.schedule == UnitbyUnit)
+    {
+        //make a new set from every unit;
+        std::vector<SelectionSet> new_selections;
+        for (auto &set : set_selection_program.selections)
+        {
+            for (auto &unit : set)
+            {
+                new_selections.push_back({unit});
+            }
+        }
+        selections = new_selections;
+    }
+    int set_count = 0;
+    bool finished = false;
+    for (auto &set : selections)
+    {
+        if (finished)
+            break;
+        bool local_finished = false;
+        std::vector<double> local_max;
+        float local_max_metr = 0;
+        int par_count = set.size();
+        if (par_count == 0)
+            continue;
+        std::map<std::string, int> par_data_pos_by_name;
+        int cur_pos = 0;
+        for (auto &desk : mask)
+        {
+            if (desk.mask != ParameterMaskValues::CONSTANT)
+            {
+                par_data_pos_by_name.emplace(desk.name,cur_pos);
+            }
+            cur_pos += desk.var_count;
+        }
+
+        std::vector<ParamTmp> valid_params;
+        int next_offset = 1;
+        for (int i=0;i<par_count;i++)
+        {
+            if (set[i].base_values_set.empty())
+                continue;
+            auto it = par_data_pos_by_name.find(set[i].parameter_name);
+            if (it != par_data_pos_by_name.end())
+            {
+                ParamTmp ptmp;
+                ptmp.offset = next_offset;
+                ptmp.data_pos = it->second;
+                ptmp.values = set[i].base_values_set;
+                ptmp.count = set[i].base_values_set.size();
+                ptmp.name = it->first;
+                valid_params.push_back(ptmp);
+                next_offset *= ptmp.count;
+            }
+            else
+            {
+                debugl(4, "warning: parameter %s in selection set is constant or does not exist\n",
+                       set[i].parameter_name);
+            }
+        }
+        int val_count = next_offset;
+        set_count++;
+        debugl(4, "Polycentric simulated annealing parameter selection set %d with %d parameters totally\n",set_count,val_count);
+
+        const int print_iter = 10;
+        std::multiset<SACenter, compare> centers;
+        std::vector<bool> visited = {};
+        if (set_selection_program.schedule == SetbySetRandomized)
+        {
+            visited = std::vector<bool>(val_count, false);
+        }
+
+        for (int i = 0; i<val_count;i++)
+        {
+            int val_id = i;
+            if (set_selection_program.schedule == SetbySetRandomized)
+            {
+                int rnd_id = urand(0, val_count);
+                if (visited[rnd_id])
+                {
+                    bool all_visited = true;
+                    int add = 1;
+                    while (all_visited)
+                    {
+                        if (rnd_id - add >= 0 && !visited[rnd_id - add])
+                        {
+                            all_visited = false;
+                            val_id = rnd_id - add;
+                        }
+                        else
+                        if (rnd_id + add < val_count && !visited[rnd_id + add])
+                        {
+                            all_visited = false;
+                            val_id = rnd_id + add;
+                        }
+                        add++;
+                    }
+                }
+                else
+                {
+                    val_id = rnd_id;
+                }
+                visited[val_id] = true;
+                //logerr("%d visited", val_id);
+            }
+            for (auto &ptmp : valid_params)
+            {
+                int num = val_id / ptmp.offset % ptmp.count;
+                data[ptmp.data_pos] = ptmp.values[num];
+            }
+
+            textureManager.set_textures_tag(1);
+            GrovePacked tree = GrovePacked();
+            param.load_from_mask_and_data(mask, data);
+            generate(param, tree);
+            global_iters++;
+            float metr = metric->get(tree);
+            centers.emplace(SACenter(val_id,metr));
+            /*if (metr > local_max_metr)
+            {
+                local_max_metr = metr;
+                local_max = data;
+            }*/
+            textureManager.clear_unnamed_with_tag(1);
+
+            std::chrono::steady_clock::time_point t_cur = std::chrono::steady_clock::now();
+            auto delta_t = t_cur - t_start;
+            float dt_sec = 1e-9*delta_t.count();
+                        
+            if ((i + 1) % print_iter == 0 || i == val_count - 1)
+            {
+                debugl(4,"preparing center %d/%d time spent %f\n",i+1,val_count,dt_sec);
+            }
+            if (dt_sec > exit_conditions.max_time_seconds)
+            {
+                finished = true;
+                debugl(4, "Maximum selection time exceeded. Finishing selection\n");
+            }
+            else if (global_iters > exit_conditions.max_iters)
+            {
+                finished = true;
+                debugl(4, "Maximum selection iteration count exceeded. Finishing selection\n");
+            }
+            else if (local_max_metr > exit_conditions.metric_reached)
+            {
+                finished = true;
+                debugl(4, "Desired quality level reached. Finishing selection\n");
+            }
+            if (finished || local_finished)
+                break;
+        }
+        
+        int centers_count = centers.size();
+        int centers_processed = 0;
+        for (const SACenter &center : centers)
+        {
+            if (local_finished || finished)
+                break;
+            param.load_from_mask_and_data(mask, data);
+            int sa_iter = 0;
+            float metr = simulated_annealing_selection(param, metric, generate, &sa_iter, 0.6, 0.1, 0.15);
+            global_iters += sa_iter;
+            if (metr > local_max_metr)
+            {
+                local_max.clear();
+                std::vector<ParameterDesc> tmp_mask;
+                param.get_mask_and_data(tmp_mask, local_max);
+                local_max_metr = metr;
+
+            }       
+
+            std::chrono::steady_clock::time_point t_cur = std::chrono::steady_clock::now();
+            auto delta_t = t_cur - t_start;
+            float dt_sec = 1e-9*delta_t.count();
+            debugl(4, "processed center %d, metr %f --> %f time spent %f\n",center.val_id, center.metr, metr, dt_sec);
+            if (dt_sec > exit_conditions.max_time_seconds)
+            {
+                finished = true;
+                debugl(4, "Maximum selection time exceeded. Finishing selection\n");
+            }
+            else if (global_iters > exit_conditions.max_iters)
+            {
+                finished = true;
+                debugl(4, "Maximum selection iteration count exceeded. Finishing selection\n");
+            }
+            else if (local_max_metr > exit_conditions.metric_reached)
+            {
+                finished = true;
+                debugl(4, "Desired quality level reached. Finishing selection\n");
+            }
+            if ((float)centers_processed/centers_count > exit_conditions.part_of_set_covered)
+            {
+                local_finished = true;
+                debugl(4, "Specified part of values tested. Moving to next set\n");
+            }
+            centers_processed++;
+        }
+
+        if (local_max_metr > max_metr)
+        {
+            data_max = local_max;
+            max_metr = local_max_metr;
+        }
+        data = data_max;
+        global_centers += centers_processed;
+        debugl(4, "parameter set %d processed local_max = %f, global_max = %f\n",
+               set_count, local_max_metr, max_metr);
+    }
+
+    std::chrono::steady_clock::time_point t_cur = std::chrono::steady_clock::now();
+    auto delta_t = t_cur - t_start;
+    float dt_sec = 1e-9*delta_t.count();
+
+    debugl(4, "\nPolycentric simulated allealing finished\n");
+    debugl(4, "Max metric: %f\n",max_metr);
+    debugl(4, "Parameter sets processed: %d\n",(int)selections.size());
+    debugl(4, "Centers tested: %d\n",(int)global_centers);
+    debugl(4, "Trees generated: %d\n",(int)global_iters);
+    debugl(4, "Time spent: %d h %d m %d s\n",(int)dt_sec/3600, ((int)dt_sec)/60 % 60, ((int)dt_sec)%60);
+
+
+    param.load_from_mask_and_data(mask, data_max);
+    return max_metr;
 }
 void ParameterSelector::select(TreeStructureParameters &param, SelectionType sel_type, MetricType metric_type)
 {
@@ -359,19 +642,20 @@ void ParameterSelector::select(TreeStructureParameters &param, SelectionType sel
         //float m = simulated_annealing_selection(param,metric,generate);
         SetSelectionProgram set_p;
         set_p.schedule = SelectionSchedule::SetbySetRandomized;
-        SelectionSet set;
+        SelectionSet set, set3;
         SelectionUnit u1;
         u1.parameter_name = "base_branch_feed";
-        u1.base_values_set = {50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150};
+        u1.base_values_set = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150};
         SelectionUnit u3;
         u3.parameter_name = "base_seg_feed";
         u3.base_values_set = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150};
-        set = {u1,u3};
+        set = {u1, u3};
+        set3 = {u3};
         set_p.selections = {set};
         ExitConditions ec;
-        ec.max_iters = 100;
-        ec.part_of_set_covered = 0.4;
-        float m = brute_force_selection(param, metric, generate, set_p, ec);
+        ec.max_iters = 1000;
+        ec.part_of_set_covered = 0.15;
+        float m = simulated_annealing_selection(param, metric, generate, set_p, ec);
         logerr("simulated annealing parameter selection finished with max_metric %f", m);
     }
 }
