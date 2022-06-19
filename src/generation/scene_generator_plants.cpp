3039955703a01febf2d4439b9eb75f7722792ea6
Sammael <Sammael>
2022-06-19 23:44:53 +0300

    Посадка деревьев 3. Поддержание воксельных массивов.

--- src/generation/scene_generator_plants.cpp
+++ src/generation/scene_generator_plants.cpp
@@ -28,27 +28,23 @@ namespace scene_gen
 
   std::mutex ctx_lock;
   std::vector<atomwrapper<bool>> thread_finished;
-  LightVoxelsCube *create_grove_voxels(GrovePrototype &prototype, std::vector<TreeTypeData> &types,
+  LightVoxelsCube *create_grove_voxels(std::vector<GrovePrototype> &prototypes, std::vector<TreeTypeData> &types,
                                       AABB &influence_box)
   {
     float min_scale_factor = 1000;
-    for (auto &p : prototype.possible_types)
+    for (auto &prototype : prototypes)
     {
-      auto &type = types[p.first];
-      min_scale_factor = MIN(min_scale_factor,type.get_params()->get_scale_factor());
+      for (auto &p : prototype.possible_types)
+      {
+        auto &type = types[p.first];
+        min_scale_factor = MIN(min_scale_factor,type.get_params()->get_scale_factor());
+      }
     }
     float vox_scale = 0.5/0.8;
     glm::vec3 voxel_sz = 0.5f*(influence_box.max_pos - influence_box.min_pos);
     glm::vec3 voxel_center = influence_box.min_pos + voxel_sz;
-    auto *v = new LightVoxelsCube(voxel_center, voxel_sz, vox_scale*min_scale_factor, 1.0f);
-    AABB &box = influence_box;
-    float Mvoxels = 1e-6*v->get_size_cnt();
-    debugl(1, "created voxels array [%.1f %.1f %.1f] - [%.1f %.1f %.1f] for patch [%.1f %.1f] - [%.1f %.1f] with %.2f Mvoxels\n",
-    box.min_pos.x,box.min_pos.y,
-    box.min_pos.z, box.max_pos.x,box.max_pos.y,box.max_pos.z,prototype.pos.x - prototype.size.x,
-    prototype.pos.y - prototype.size.y, prototype.pos.x + prototype.size.x, prototype.pos.y + prototype.size.y,
-    Mvoxels);
-    return v;
+
+    return new LightVoxelsCube(voxel_center, voxel_sz, vox_scale*min_scale_factor, 1.0f);
   }
 
   LightVoxelsCube *create_cell_small_voxels(Cell &c, SceneGenerator::SceneGenerationContext &ctx)
@@ -213,38 +209,29 @@ namespace scene_gen
     std::vector<Cell> &cells;
 
     std::list<int> waiting_cells;
-    std::list<int> border_cells;
     GroveGenerationData ggd;
     RawTreesDatabase &rawTreesDatabase;
-
-    GrassGenerator &grassGenerator;
-    std::mutex grassGenerator_lock;
-
     GroveMask &mask;
 
     int cells_x;
     int cells_y;
-    bool grass_needed;
     int id;
     GenerationJob(SceneGenerator::SceneGenerationContext &_ctx, std::vector<Cell> &_cells,
-                  std::vector<TreeTypeData> &_types, RawTreesDatabase &_database, GrassGenerator &_grassGenerator,
-                  GroveMask &_mask, int _cells_x, int _cells_y, bool _grass_needed, int _id) : ctx(_ctx),
+                  std::vector<TreeTypeData> &_types, RawTreesDatabase &_database,
+                  GroveMask &_mask, int _cells_x, int _cells_y, int _id) : ctx(_ctx),
                                                                   cells(_cells),
                                                                   rawTreesDatabase(_database),
-                                                                  grassGenerator(_grassGenerator),
                                                                   mask(_mask)
     {
       ggd.types = _types;
       cells_x = _cells_x;
       cells_y = _cells_y;
-      grass_needed = _grass_needed;
       id = _id;
     };
     GenerationJob(const GenerationJob &) = delete;
     GenerationJob(GenerationJob &) = delete;
     void prepare_dependencies(); //done in main thread;
-    void generate();//can be done in separate thread     
-    bool remove_unused_borders();      
+    void generate();//can be done in separate thread           
   };
   void GenerationJob::prepare_dependencies()
   {
@@ -304,8 +291,7 @@ namespace scene_gen
   {
     for (int c_id : waiting_cells)
     {
-      //generation trees and grass
-      bool short_lived_voxels = false;
+      //generation trees
       auto &c = cells[c_id];
       auto influence_bb = c.influence_bbox;
 
@@ -315,21 +301,21 @@ namespace scene_gen
       
       if (tc > 0)
       {
-        //temp stuff
-        GrovePrototype base_prototype = c.prototypes[0];//all prototypes should have the same size
-        ggd.pos.x = base_prototype.pos.x;
-        ggd.pos.z = base_prototype.pos.y;
+        logerr("cell %d creating %d trees in %d tree prototypes", c.id, tc, c.prototypes.size());
+
+        ggd.pos.x = 0.5f * (c.bbox.max_pos.x + c.bbox.min_pos.x);
+        ggd.pos.z = 0.5f * (c.bbox.max_pos.y + c.bbox.min_pos.y);
 
         ctx_lock.lock();
         ggd.pos.y = ctx.scene->heightmap->get_height(ggd.pos);
         ctx_lock.unlock();
 
-        ggd.size.x = base_prototype.size.x;
-        ggd.size.z = base_prototype.size.y;
-        ggd.trees_count = base_prototype.trees_count;
+        ggd.size.x = 0.5f * (c.bbox.max_pos.x - c.bbox.min_pos.x);
+        ggd.size.z = 0.5f * (c.bbox.max_pos.y - c.bbox.min_pos.y);
+        ggd.trees_count = tc;
 
         GroveGenerator grove_gen;
-        LightVoxelsCube *voxels = create_grove_voxels(base_prototype, ggd.types, influence_bb);
+        LightVoxelsCube *voxels = create_grove_voxels(c.prototypes, ggd.types, influence_bb);
         
         ctx_lock.lock();
         auto func = [&](const std::pair<AABB, uint64_t> &p)
@@ -341,6 +327,7 @@ namespace scene_gen
 
         c.cell_lock.lock();
         std::vector<int> deps = c.depends_from;
+        voxels->add_voxels_cube(c.voxels_small);
         c.cell_lock.unlock();
         for (auto &dep_cid : deps)
         {
@@ -367,138 +354,27 @@ namespace scene_gen
           rawTreesDatabase.generation_finished(token);
           rawTreesDatabase.database_lock.unlock();
         }
+        
         c.cell_lock.lock();
-        if (!c.depends.empty())
-        {
-          c.status = Cell::CellStatus::BORDER;
-          c.voxels_small = new LightVoxelsCube(voxels,glm::ivec3(0,0,0), voxels->get_vox_sizes(),
-                                              voxels->get_block_size() > 1 ? voxels->get_block_size() : 5,
-                                              glm::vec2(0,1e8));
-          border_cells.push_back(c_id);
-        }
-        else
-        {
-          c.status = Cell::CellStatus::FINISHED_PLANTS;
-        }
+        if (c.voxels_small)
+          delete c.voxels_small;
+        c.voxels_small = new LightVoxelsCube(voxels,glm::ivec3(0,0,0), voxels->get_vox_sizes(),
+                                             voxels->get_block_size() > 1 ? voxels->get_block_size() : 5,
+                                             glm::vec2(0,1e8));
         c.cell_lock.unlock();
-        if (debugTransferSettings.save_detailed_voxels_count > debugTransferData.debug_voxels.size())
-          debugTransferData.debug_voxels.push_back(voxels);
-        else
-          delete voxels;
-      } 
-      else 
-      {
-        c.cell_lock.lock();
-        if (!c.depends.empty())
-        {
-          c.status = Cell::CellStatus::BORDER;
-          c.voxels_small = create_cell_small_voxels(c, ctx);
-          border_cells.push_back(c_id);
-        }
-        else
-        {
-          c.status = Cell::CellStatus::FINISHED_PLANTS;
-        }
-        c.cell_lock.unlock();
-      }
 
-      //TODO: can be done not every iteration
-      remove_unused_borders();
-      //logerr("finished generating cell %d", c_id);
+        delete voxels;
+      } 
+
+      c.cell_lock.lock();
+      c.prototypes.clear();
+      c.status = Cell::CellStatus::FINISHED_PLANTS;
+      c.cell_lock.unlock();
     }
-    int tries = 0;
-    int max_tries = 100;
-    while (!remove_unused_borders())
-    {
-      tries = MIN(tries + 1, max_tries);
-      for (int t=0;t<tries;t++)
-        std::this_thread::yield();
-    }
-    if (grass_needed)
-    {
-      auto it = waiting_cells.begin();
-      while (it != waiting_cells.end())
-      {
-        if (!cells[*it].planar_occlusion)
-        {
-          AABB2D bbox = cells[*it].bbox;
-          glm::vec3 occ_center = 0.5f * glm::vec3(bbox.max_pos.x + bbox.min_pos.x, 0, bbox.max_pos.y + bbox.min_pos.y);
-          glm::vec2 bord = glm::vec2(bbox.max_pos.x - bbox.min_pos.x, bbox.max_pos.y - bbox.min_pos.y);
-          float vox_size = MIN(bord.x, bord.y) / 64;
-          cells[*it].planar_occlusion = new Field_2d(occ_center, 0.5f * bord, vox_size);
-          std::function<float(glm::vec2 &)> func = [&](glm::vec2 &p) -> float
-          {
-            glm::vec3 pos = glm::vec3(p.x, 0, p.y);
-            pos.y = ctx.scene->heightmap->get_height(pos) + 1;
-            if (ctx.objects_bvh.contains(pos))
-              return 1e9;
-            else
-              return 0;
-          };
-          cells[*it].planar_occlusion->fill_func(func);
-        }
-        it++;
-      }
-    }
+
     thread_finished[id]._a.store(true);
   }
 
-  bool GenerationJob::remove_unused_borders()
-  {
-    auto it = border_cells.begin();
-    while (it != border_cells.end())
-    {
-      bool have_deps = false;
-      cells[*it].cell_lock.lock();
-      std::vector<int> deps = cells[*it].depends;
-      cells[*it].cell_lock.unlock();
-      for (int &dep : deps)
-      {
-        if (cells[dep].status.load() == Cell::CellStatus::WAITING)
-        {
-          have_deps = true;
-          break;
-        }
-      }
-      if (!have_deps)
-      {
-        cells[*it].cell_lock.lock();
-        //logerr("removed dependency %d",*it);
-        if (grass_needed)
-        {
-          if (cells[*it].voxels_small)
-          {
-            glm::vec3 occ_center = cells[*it].voxels_small->get_center();
-            AABB bbox = cells[*it].voxels_small->get_bbox();
-            glm::vec2 bord = glm::vec2(bbox.max_pos.x - bbox.min_pos.x, bbox.max_pos.z - bbox.min_pos.z);
-            float vox_size = cells[*it].voxels_small->get_voxel_size();
-            cells[*it].planar_occlusion = new Field_2d(occ_center, 0.5f * bord, vox_size);
-            std::function<float(glm::vec2 &)> func = [&](glm::vec2 &p) -> float
-            {
-              return cells[*it].voxels_small->get_occlusion_projection(glm::vec3(p.x, 0, p.y));
-            };
-            cells[*it].planar_occlusion->fill_func(func);
-          }
-        }
-        cells[*it].status = Cell::CellStatus::FINISHED_PLANTS;
-
-        if (debugTransferSettings.save_small_voxels_count > debugTransferData.debug_voxels.size())
-          debugTransferData.debug_voxels.push_back(cells[*it].voxels_small);
-        else
-          delete cells[*it].voxels_small;
-
-        cells[*it].voxels_small = nullptr;
-        cells[*it].cell_lock.unlock();
-        it = border_cells.erase(it);
-      }
-      else
-      {
-        it++;
-      }
-    }
-
-    return border_cells.empty();
-  }
   void generate_plants_cells(SceneGenerator::SceneGenerationContext &ctx, std::vector<int> all_cell_ids)
   {
     std::vector<int> cell_ids;//only valid cells where we have something to generate
@@ -529,19 +405,14 @@ namespace scene_gen
     int jobs_cnt = MAX(ceil((float)cell_ids.size()/job_size), 1);
     logerr("starting generation %d cells with %d jobs (job size = %d)", cell_ids.size(), jobs_cnt, job_size);
 
-    GrassGenerator grassGenerator;//tmp
-    bool grass_needed = false;//tmp
 
-    glm::vec2 mask_pos = ctx.center;
-    glm::vec2 mask_size = ctx.grass_field_size;
-    GroveMask global_mask = GroveMask(glm::vec3(mask_pos.x,0,mask_pos.y), mask_size, ctx.biome_map_pixel_size);
+    GroveMask global_mask = GroveMask(glm::vec3(ctx.center.x,0,ctx.center.y), ctx.grass_field_size, ctx.biome_map_pixel_size);
     global_mask.fill_const(1);
 
     thread_finished = {};
     for (int i=0;i<jobs_cnt;i++)
     {
-      generationJobs.push_back(new GenerationJob(ctx, cells, types, rawTreesDatabase, grassGenerator, global_mask, 
-                                                 ctx.cells_x, ctx.cells_y, grass_needed, i));
+      generationJobs.push_back(new GenerationJob(ctx, cells, types, rawTreesDatabase, global_mask, ctx.cells_x, ctx.cells_y, i));
       std::atomic<bool> ab(false);
       thread_finished.push_back(ab);                                      
     }
@@ -593,25 +464,9 @@ namespace scene_gen
       }
     }
     for (auto *j : generationJobs)
-    {
       delete j;
-    }
 
     rawTreesDatabase.pack_ready(packer, ctx, true);
-
-    for (auto &id : cell_ids)
-    {
-      auto &c = cells[id];
-      if (c.planar_occlusion)
-        delete c.planar_occlusion;
-      if (c.voxels_small)
-      {
-        logerr("missed dependencies. Cell %d", c.id);
-        delete c.voxels_small;
-      }
-      c.prototypes.clear();
-    }
-
     packer.prepare_grove_atlas(ctx.scene->grove, 512, 512, true, true, true);
   }
 };
