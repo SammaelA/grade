5b260dae6e9dac98a0544fffea104dbde51c1bf3
Zallerrazion <maiorov.kolyan@yandex.ru>
2023-03-12 14:15:08 +0300

    new algorithm for iou3d (wihtout tests)

--- src/diff_generators/iou3d.cpp
+++ src/diff_generators/iou3d.cpp
@@ -1,9 +1,63 @@
 #include "iou3d.h"
-#include "diff_geometry_generation.h"
-#include "common_utils/interpolation.h"
-#include <cppad/cppad.hpp>
+#include <cstdlib>
+#include <ctime>
 
-double iou3d(const std::vector<dfloat> &model1, const std::vector<dfloat> &model2, 
+bool inter_tr_line(double x1, double y1, double z1,
+  double x2, double y2, double z2,
+  double x3, double y3, double z3,
+  double x, double y, double z,
+  double x_v, double y_v, double z_v)
+{
+  double matr[4][3];
+  matr[0][0] = x1 - x2;
+  matr[0][1] = y1 - y2;
+  matr[0][2] = z1 - z2;
+  matr[1][0] = x1 - x3;
+  matr[1][1] = y1 - y3;
+  matr[1][2] = z1 - z3;
+  matr[2][0] = x_v;
+  matr[2][1] = y_v;
+  matr[2][2] = z_v;
+  matr[3][0] = x1 - x;
+  matr[3][1] = y1 - y;
+  matr[3][2] = z1 - z;
+  double a, b, c;
+  for (int k = 0; k < 3; ++k)
+  {
+    for (int i = k; i < 3; ++i)
+    {
+      if (matr[k][i] != 0)
+      {
+        double div = matr[k][i];
+        for (int j = 0; j < 4; ++j)
+        {
+          double tmp = matr[j][i] / div;
+          matr[j][i] = matr[j][k];
+          matr[j][k] = tmp;
+        }
+        for (int j = 0; j < 3; ++j)
+        {
+          double div = matr[k][j];
+          for (int u = 0; u < 4; ++u)
+          {
+            if (j != k && div != 0)
+            {
+              matr[u][j] = matr[u][j] / div - matr[u][k];
+            }
+          }
+        }
+        break;
+      }
+      if (i == 2)
+      {
+        return false;
+      }
+    }
+  }
+  return matr[3][0] >= 0 && matr[3][0] <= 1 && matr[3][1] >= 0 && matr[3][1] <= 1 && matr[3][1] + matr[3][0] <= 1 && matr[3][2] >= 0;
+}
+
+double iou3d(const std::vector<float> &model1, const std::vector<float> &model2, 
             double x_s, double y_s, double z_s, double x_e, double y_e, double z_e, double step)
 {
   double intersect = 0, interconnect = 0;
@@ -14,32 +68,32 @@ double iou3d(const std::vector<dfloat> &model1, const std::vector<dfloat> &model
     {
       for (double z = z_s; z <= z_e; z += step)
       {
-        double dist = -1;
         int num1 = 0, num2 = 0;
-        double tmp = 0;
-        for (int i = 0; i < model1.size(); i += FLOAT_PER_VERTEX)
+        double x_vec = rand() % 2 + 0.5;
+        double y_vec = rand() % 2 + 0.5;
+        double z_vec = rand() % 2 + 0.5;
+        for (int i = 0; i < model1.size(); i += 3 * FLOAT_PER_VERTEX)
         {
-          tmp = pow(model1[i + 0] - x, 2) + pow(model1[i + 1] - y, 2) + pow(model1[i + 2] - z, 2);
-          if (dist == -1 || tmp < dist || 
-              (tmp == dist && (x - model1[i + 0]) * model1[i + 3] + (y - model1[i + 1]) * model1[i + 4] + (x - model1[i + 2]) * model1[i + 5] != 0))
+          if (inter_tr_line(model1[i], model1[i + 1], model1[i + 2], 
+              model1[i + FLOAT_PER_VERTEX], model1[i + 1 + FLOAT_PER_VERTEX], model1[i + 2 + FLOAT_PER_VERTEX], 
+              model1[i + FLOAT_PER_VERTEX * 2], model1[i + 1 + FLOAT_PER_VERTEX * 2], model1[i + 2 + FLOAT_PER_VERTEX * 2], 
+              x, y, z, x_vec, y_vec, z_vec))
           {
-            dist = tmp;
-            num1 = i;
+            ++num1;
           }
         }
-        dist = -1;
-        for (int i = 0; i < model2.size(); i += FLOAT_PER_VERTEX)
+        for (int i = 0; i < model2.size(); i += 3 * FLOAT_PER_VERTEX)
         {
-          tmp = pow(model2[i + 0] - x, 2) + pow(model2[i + 1] - y, 2) + pow(model2[i + 2] - z, 2);
-          if (dist == -1 || tmp < dist || 
-              (tmp == dist && (x - model2[i + 0]) * model2[i + 3] + (y - model2[i + 1]) * model2[i + 4] + (x - model2[i + 2]) * model2[i + 5] != 0))
+          if (inter_tr_line(model2[i], model2[i + 1], model2[i + 2], 
+              model2[i + FLOAT_PER_VERTEX], model2[i + 1 + FLOAT_PER_VERTEX], model2[i + 2 + FLOAT_PER_VERTEX], 
+              model2[i + FLOAT_PER_VERTEX * 2], model2[i + 1 + FLOAT_PER_VERTEX * 2], model2[i + 2 + FLOAT_PER_VERTEX * 2], 
+              x, y, z, x_vec, y_vec, z_vec))
           {
-            dist = tmp;
-            num2 = i;
+            ++num2;
           }
         }
-        bool prod1 = (x - model1[num1 + 0]) * model1[num1 + 3] + (y - model1[num1 + 1]) * model1[num1 + 4] + (x - model1[num1 + 2]) * model1[num1 + 5] <= 0;
-        bool prod2 = (x - model2[num2 + 0]) * model2[num2 + 3] + (y - model2[num2 + 1]) * model2[num2 + 4] + (x - model2[num2 + 2]) * model2[num2 + 5] <= 0;
+        bool prod1 = (num1 % 2 == 1);
+        bool prod2 = (num2 % 2 == 1);
         if (prod1 && prod2)
         {
           intersect += 1;
--- src/diff_generators/iou3d.h
+++ src/diff_generators/iou3d.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "differentiable_generators.h"
 
-namespace dgen
-{
-  double iou3d(const std::vector<dfloat> &model1, const std::vector<dfloat> &model2);
-}
+
+double iou3d(const std::vector<float> &model1, const std::vector<float> &model2, 
+            double x_s, double y_s, double z_s, double x_e, double y_e, double z_e, double step);
