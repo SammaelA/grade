cd72c973e0946abea509b9a33bcb6cd52412d6fe
Sammael <Sammael>
2023-11-05 22:36:37 +0300

    tests for simple optimization cases

--- src/UPG_reconstruction/tests.cpp
+++ src/UPG_reconstruction/tests.cpp
@@ -2,12 +2,13 @@
 
 namespace upg
 {
+
   //TEST 1 ONE TRIANGLE RECONSTRUCTION
   //It uses Adam optimizer with initial state close to target one
   //Reconstruction should perform perfectly (like 90 PSNR)
   void test_1()
   {
-    debug("TEST 1. ONE TRIANGLE RECONSTRUCTION: ");
+    debug("TEST 1. ONE TRIANGLE SINGLE-VIEW RECONSTRUCTION\n");
     std::string settings = R""""(
     {
     input {
@@ -73,18 +74,247 @@ namespace upg
     Block settings_blk;
     load_block_from_string(settings, settings_blk);
     auto res = reconstruct(settings_blk);
-    if (res.empty())
-      debug("FAILED. No reconstruction result returned");
-    else if (res[0].quality_ir < 80)
-      debug("FAILED. Low PSNR (%.1f < 80.0(target))", res[0].quality_ir);
+
+    debug("  1.1. %-64s", "ReconstructionResult size ");
+    if (res.size() == 1)
+      debug("PASSED\n");
     else
-      debug("PASSED.");
-    debugnl();
+      debug("FAILED %d != %d\n", res.size(), 1);
+    
+    debug("  1.2. %-64s", "Preserved structure ");
+    if (res[0].structure.s.size() == 1 && res[0].structure.s[0] == 1)
+      debug("PASSED\n");
+    else
+      debug("FAILED\n");
+    
+    debug("  1.3. %-64s", "Preserved parameters count ");
+    if (res[0].parameters.p.size() == 9)
+      debug("PASSED\n");
+    else
+      debug("FAILED\n");
+    
+    debug("  1.4. %-64s", "Perfect optimization loss ");
+    if (res[0].loss_optimizer < 1e-5)
+      debug("PASSED\n");
+    else
+      debug("FAILED %f > %f\n", res[0].loss_optimizer, 1e-5);
+
+    debug("  1.5. %-64s", "Perfect one-view PSNR ");
+    if (res[0].quality_ir > 80)
+      debug("PASSED\n");
+    else
+      debug("FAILED %f < %f\n", res[0].quality_ir, 80);
+
+    debug("  1.6. %-64s", "Adequate turntable PSNR ");
+    if (res[0].quality_synt > 10)
+      debug("PASSED\n");
+    else
+      debug("FAILED %f < %f\n", res[0].quality_synt, 10);
+
+    ComplexModel mod;
+    bool cm_res = create_model(res[0].structure, res[0].parameters, mod);
+    debug("  1.7. %-64s", "Reconstructed model can be created ");
+    if (cm_res)
+      debug("PASSED\n");
+    else
+      debug("FAILED\n");
+
+    debug("  1.8. %-64s", "Reconstructed model is valid ");
+    if (mod.materials.size() == 1 && mod.models.size() == 1 && mod.models[0] && mod.models[0]->positions.size() == 3*3 && 
+        mod.models[0]->normals.size() == 3*3 && mod.models[0]->colors.size() == 4*3)
+      debug("PASSED\n");
+    else
+      debug("FAILED %d %d %d %d %d %d\n", mod.materials.size() == 1, mod.models.size() == 1, mod.models[0], 
+            mod.models[0] ? (mod.models[0]->positions.size() == 3*3) : 0, 
+            mod.models[0] ? (mod.models[0]->normals.size() == 3*3) : 0,
+            mod.models[0] ? (mod.models[0]->colors.size() == 4*3) : 0);    
   }
 
+  //TEST 2 ONE TRIANGLE RECONSTRUCTION
+  //It uses Adam optimizer with initial state close to target one
+  //Reconstruction should perform perfectly (like 90 PSNR)
+  void test_2()
+  {
+    debug("TEST 2. ONE TRIANGLE MULTI-VIEW RECONSTRUCTION\n");
+    std::string settings = R""""(
+    {
+    input {
+        synthetic_reference {
+            tex_w:i = 512
+            tex_h:i = 512
+            params:arr = {0,0,0, -1,0,0, 0,1,-1}
+            structure:arr = {1}
+        } 
+        view_0 {
+            camera.origin:p3 = 2.000000, 0.500000, 2.000000
+            camera.target:p3 = 0.000000, 0.000000, 0.000000
+            camera.up:p3 = 0.000000, 1.000000, 0.000000
+            camera.z_near:r = 0.100000
+            camera.z_far:r = 100.000000
+            camera.fov_rad:r = 1.00000
+            camera.fixed:b = true
+        }
+        view_1 {
+            camera.origin:p3 = 2.000000, -0.500000, -2.000000
+            camera.target:p3 = 0.000000, 0.000000, 0.000000
+            camera.up:p3 = 0.000000, 1.000000, 0.000000
+            camera.z_near:r = 0.100000
+            camera.z_far:r = 100.000000
+            camera.fov_rad:r = 1.00000
+            camera.fixed:b = true
+        }
+        view_2 {
+            camera.origin:p3 = -2.000000, 0.500000, 2.000000
+            camera.target:p3 = 0.000000, 0.000000, 0.000000
+            camera.up:p3 = 0.000000, 1.000000, 0.000000
+            camera.z_near:r = 0.100000
+            camera.z_far:r = 100.000000
+            camera.fov_rad:r = 1.00000
+            camera.fixed:b = true
+        }
+        view_3 {
+            camera.origin:p3 = -2.000000, -0.500000, -2.000000
+            camera.target:p3 = 0.000000, 0.000000, 0.000000
+            camera.up:p3 = 0.000000, 1.000000, 0.000000
+            camera.z_near:r = 0.100000
+            camera.z_far:r = 100.000000
+            camera.fov_rad:r = 1.00000
+            camera.fixed:b = true
+        }
+    }
+    generator {
+
+    }
+    optimization {
+        start {
+            params:arr = {0.1,0.1,0.1, -0.9,-0.1,-0.05, 0.07,0.85,-0.81}    
+            structure:arr = {1} 
+        }
+        step_0 {
+            render_w:i = 512
+            render_h:i = 512
+            iterations:i = 500
+            verbose:b = false
+            save_intermediate_images:b = false
+            learning_rate:r = 0.003
+        }
+    }
+    results {
+        check_image_quality:b = true
+        check_model_quality:b = true
+    }
+    }
+      )"""";
+    Block settings_blk;
+    load_block_from_string(settings, settings_blk);
+    auto res = reconstruct(settings_blk);
+
+    debug("  2.1. %-64s", "Perfect optimization loss ");
+    if (res[0].loss_optimizer < 1e-5)
+      debug("PASSED\n");
+    else
+      debug("FAILED %f > %f\n", res[0].loss_optimizer, 1e-5);
+
+    debug("  2.2. %-64s", "Extremely high PSNR on given views ");
+    if (res[0].quality_ir > 50)
+      debug("PASSED\n");
+    else
+      debug("FAILED %f < %f\n", res[0].quality_ir, 50);
+
+    debug("  2.3. %-64s", "Extremely high turntable PSNR ");
+    if (res[0].quality_synt > 40)
+      debug("PASSED\n");
+    else
+      debug("FAILED %f < %f\n", res[0].quality_synt, 40);
+  }
+
+  //TEST 3 ONE TRIANGLE RECONSTRUCTION
+  //It uses Adam optimizer with initial state close to target one
+  //Reconstruction should perform perfectly (like 90 PSNR)
+  void test_3()
+  {
+    debug("TEST 3. ONE TRIANGLE RECONSTRUCTION FROM MASK\n");
+    std::string settings = R""""(
+    {
+    input {
+        synthetic_reference {
+            tex_w:i = 256
+            tex_h:i = 256
+            params:arr = {0,0,0, -1,0,0, 0,1,-1}
+            structure:arr = {1}
+        } 
+        view_0 {
+            camera.origin:p3 = 0.000000, 0.000000, 3.000000
+            camera.target:p3 = 0.000000, 0.000000, 0.000000
+            camera.up:p3 = 0.000000, 1.000000, 0.000000
+            camera.z_near:r = 0.100000
+            camera.z_far:r = 100.000000
+            camera.fov_rad:r = 1.00000
+            camera.fixed:b = true
+            mask:s = "saves/UPG_tests/test_3_input.png"
+        }
+    }
+    generator {
+
+    }
+    optimization {
+        start {
+            params:arr = {0.1,0.1,0.1, -0.9,-0.1,-0.05, 0.07,0.85,-0.81}    
+            structure:arr = {1} 
+        }
+        step_0 {
+            render_w:i = 256
+            render_h:i = 256
+            iterations:i = 100
+            verbose:b = false
+            save_intermediate_images:b = false
+        }
+    }
+    results {
+        check_image_quality:b = true
+        check_model_quality:b = true
+    }
+    }
+      )"""";
+    Block settings_blk;
+    load_block_from_string(settings, settings_blk);
+    auto res = reconstruct(settings_blk);
+
+    debug("  3.1. %-64s", "ReconstructionResult size ");
+    if (res.size() == 1)
+      debug("PASSED\n");
+    else
+      debug("FAILED %d != %d\n", res.size(), 1);
+    
+    debug("  3.2. %-64s", "Preserved structure ");
+    if (res[0].structure.s.size() == 1 && res[0].structure.s[0] == 1)
+      debug("PASSED\n");
+    else
+      debug("FAILED\n");
+    
+    debug("  3.3. %-64s", "Preserved parameters count ");
+    if (res[0].parameters.p.size() == 9)
+      debug("PASSED\n");
+    else
+      debug("FAILED\n");
+    
+    debug("  3.4. %-64s", "Perfect optimization loss ");
+    if (res[0].loss_optimizer < 1e-5)
+      debug("PASSED\n");
+    else
+      debug("FAILED %f > %f\n", res[0].loss_optimizer, 1e-5);
+
+    debug("  3.5. %-64s", "Perfect one-view PSNR ");
+    if (res[0].quality_ir > 80)
+      debug("PASSED\n");
+    else
+      debug("FAILED %f < %f\n", res[0].quality_ir, 80);
+  }
 
   void perform_tests()
   {
     test_1();
+    test_2();
+    test_3();
   }
 };
