956f03f82145b3242eac1540d259af1c58913f84
Sammael <Sammael>
2023-12-17 12:50:04 +0300

    fixed autodiff stub. After recent commit from Sergei project fails to compile without enzyme

--- modules/custom_diff_render/autodiff_stub.cpp
+++ modules/custom_diff_render/autodiff_stub.cpp
@@ -5,6 +5,21 @@ void __enzyme_fwddiff(void*, ...)
 {
   logerr("custom_diff_render module is not included. Change settings in CMakeLists.txt to use it.");
 }
+float __enzyme_autodiffFloat(float (*)(float), float)
+{
+  logerr("custom_diff_render module is not included. Change settings in CMakeLists.txt to use it.");
+}
+
+double __enzyme_autodiffDouble(double (*)(double), double)
+{
+  logerr("custom_diff_render module is not included. Change settings in CMakeLists.txt to use it.");
+}
+
+float __enzyme_autodiff(...)
+{
+  logerr("custom_diff_render module is not included. Change settings in CMakeLists.txt to use it.");
+}
+
 int enzyme_dupnoneed = 0;
 int enzyme_dup = 0;
 float __enzyme_v[1024] = {0};
