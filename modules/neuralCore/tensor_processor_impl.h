5b44535e34921210ca7c5a0b6e9e358cb7c74e84
Sammael <Sammael>
2024-01-05 17:58:21 +0300

    noinline attribute for kernel functions

--- modules/neuralCore/tensor_processor_impl.h
+++ modules/neuralCore/tensor_processor_impl.h
@@ -14,21 +14,21 @@ protected:
 
   virtual void CommitDeviceData() {}                                                         // will be overriden in generated class
   virtual void GetExecutionTime(const char *a_funcName, float a_out[4]) { a_out[0] = 0.0f; } // will be overriden in generated class
-  virtual void kernel1D_fill(float *data, unsigned steps, Variable A, float val); // A = val
-  virtual void kernel2D_add(float *data, unsigned steps, unsigned step_size, Variable A, Variable B, Variable C); // C = A + B
-  virtual void kernel2D_mul(float *data, unsigned steps, unsigned step_size, Variable A, Variable B, Variable C); // C = A * B
-  virtual void kernel2D_sub(float *data, unsigned steps, unsigned step_size, Variable A, Variable B, Variable C); // C = A - B
-  virtual void kernel2D_div(float *data, unsigned steps, unsigned step_size, Variable A, Variable B, Variable C); // C = A / B
-  virtual void kernel1D_exp(float *data, unsigned steps, Variable A, Variable B);                                 // B = exp(A)
-  virtual void kernel1D_pow(float *data, unsigned steps, Variable A, Variable B, Variable C);                     // C = pow(A, B)
-  virtual void kernel1D_sin(float *data, unsigned steps, Variable A, Variable B);                                 // B = sin(A)
-  virtual void kernel1D_cos(float *data, unsigned steps, Variable A, Variable B);                                 // B = cos(A)
-  virtual void kernel1D_log(float *data, unsigned steps, Variable A, Variable B);                                 // B = log(A)
-  virtual void kernel1D_sum(float *data, unsigned steps, unsigned step_size, Variable A, Variable B);             // B = sum(A)
-  virtual void kernel1D_osum(float *data, unsigned steps, unsigned step_size, Variable A, Variable B);             // B = sum(A)
-  virtual void kernel2D_transpose(float *data, unsigned steps, unsigned row_len, unsigned col_len, Variable A, Variable B); // B = (A)^T
-  virtual void kernel2D_matmul_transposed(float *data, unsigned A_row_len, unsigned A_col_len, unsigned B_col_len, 
+  virtual void __attribute__((noinline)) kernel1D_fill(float *data, unsigned steps, Variable A, float val); // A = val
+  virtual void __attribute__((noinline)) kernel2D_add(float *data, unsigned steps, unsigned step_size, Variable A, Variable B, Variable C); // C = A + B
+  virtual void __attribute__((noinline)) kernel2D_mul(float *data, unsigned steps, unsigned step_size, Variable A, Variable B, Variable C); // C = A * B
+  virtual void __attribute__((noinline)) kernel2D_sub(float *data, unsigned steps, unsigned step_size, Variable A, Variable B, Variable C); // C = A - B
+  virtual void __attribute__((noinline)) kernel2D_div(float *data, unsigned steps, unsigned step_size, Variable A, Variable B, Variable C); // C = A / B
+  virtual void __attribute__((noinline)) kernel1D_exp(float *data, unsigned steps, Variable A, Variable B);                                 // B = exp(A)
+  virtual void __attribute__((noinline)) kernel1D_pow(float *data, unsigned steps, Variable A, Variable B, Variable C);                     // C = pow(A, B)
+  virtual void __attribute__((noinline)) kernel1D_sin(float *data, unsigned steps, Variable A, Variable B);                                 // B = sin(A)
+  virtual void __attribute__((noinline)) kernel1D_cos(float *data, unsigned steps, Variable A, Variable B);                                 // B = cos(A)
+  virtual void __attribute__((noinline)) kernel1D_log(float *data, unsigned steps, Variable A, Variable B);                                 // B = log(A)
+  virtual void __attribute__((noinline)) kernel1D_sum(float *data, unsigned steps, unsigned step_size, Variable A, Variable B);             // B = sum(A)
+  virtual void __attribute__((noinline)) kernel1D_osum(float *data, unsigned steps, unsigned step_size, Variable A, Variable B);             // B = sum(A)
+  virtual void __attribute__((noinline)) kernel2D_transpose(float *data, unsigned steps, unsigned row_len, unsigned col_len, Variable A, Variable B); // B = (A)^T
+  virtual void __attribute__((noinline)) kernel2D_matmul_transposed(float *data, unsigned A_row_len, unsigned A_col_len, unsigned B_col_len, 
                                           Variable A, Variable B, Variable C);                                // C = A * (B)^T
-  virtual void kernel2D_outer_product(float *data, unsigned steps, unsigned A_len, unsigned B_len, 
-                                      Variable A, Variable B, Variable C);
+  virtual void __attribute__((noinline)) kernel2D_outer_product(float *data, unsigned steps, unsigned A_len, unsigned B_len, 
+                                         Variable A, Variable B, Variable C);
 };
