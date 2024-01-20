e93f3de8fd64842e7b8188e237ffddd20de9bef2
Sammael <Sammael>
2024-01-20 18:55:43 +0300

    updated sdf-diff-gen script

--- other/sdf_diff_gen_to_compare.sh
+++ other/sdf_diff_gen_to_compare.sh
@@ -2,16 +2,16 @@
 
 cd ~/references_grade/differentiable-sdf-rendering/python
 
-python3.8 optimize.py "$1" --optconfig diffuse-1 --force
-python3.8 render_turntable.py "$1" --optconfig diffuse-1
+python optimize.py "$1" --optconfig diffuse-1 --force
+python render_turntable.py "$1" --optconfig diffuse-1
 
-python3.8 optimize.py "$1" --optconfig diffuse-2 --force
-python3.8 render_turntable.py "$1" --optconfig diffuse-2
+python optimize.py "$1" --optconfig diffuse-2 --force
+python render_turntable.py "$1" --optconfig diffuse-2
 
-python3.8 optimize.py "$1" --optconfig diffuse-6 --force
-python3.8 render_turntable.py "$1" --optconfig diffuse-6
+python optimize.py "$1" --optconfig diffuse-6 --force
+python render_turntable.py "$1" --optconfig diffuse-6
 
-python3.8 optimize.py "$1" --optconfig diffuse-12 --force
-python3.8 render_turntable.py "$1" --optconfig diffuse-12
+python optimize.py "$1" --optconfig diffuse-12 --force
+python render_turntable.py "$1" --optconfig diffuse-12
 
 cd ~/grade
