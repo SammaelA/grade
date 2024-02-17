aaa6de73edc47f491b18b66efabad20d824c41bd
Sammael <albgar-14@yandex.ru>
2024-02-17 17:46:46 +0300

    SDF refactor 4. rendering using batches

--- src/UPG_reconstruction/sdf_rendering.cpp
+++ src/UPG_reconstruction/sdf_rendering.cpp
@@ -3,6 +3,9 @@
 #include "tinyEngine/engine.h"
 #include "common_utils/distribution.h"
 #include "preprocessing.h"
+#include <chrono>
+#include <omp.h>
+#include <thread>
 
 namespace upg
 {
@@ -88,23 +91,81 @@ namespace upg
     return t;
   }
 
-  void render_sdf_to_array(std::span<float> out_array, const ProceduralSdf &sdf, const CameraSettings &camera, int image_w, int image_h, int spp, SDFRenderMode mode)
+  void evaluate_job(const ProceduralSdf &sdf, std::span<float> positions, std::span<float> distances, 
+                    unsigned batch_size, unsigned offset, unsigned end)
   {
-    AABB sdf_bbox = sdf.root->get_bbox();
-    glm::mat4 projInv = glm::inverse(glm::perspective(camera.fov_rad, 1.0f, camera.z_near, camera.z_far));
-    glm::mat4 viewInv = glm::inverse(camera.get_view());
-    //set light somewhere to the side 
-    glm::vec3 light_dir = normalize(camera.origin + glm::vec3(camera.origin.z, camera.origin.y, camera.origin.x) - camera.target);
-    int spp_a = MAX(1,floor(sqrtf(spp)));
+    while (offset < end)
+    {
+      unsigned size = MIN(batch_size, end - offset);
+      sdf.get_distance_batch(size, positions.data() + 3*offset, distances.data() + offset, nullptr, nullptr);
+      offset += size;
+    }
+  }
 
-    //#pragma omp parallel for
+  void evaluate(const std::vector<ProceduralSdf> &sdfs, std::span<float> positions, std::span<float> distances, unsigned count)
+  {
+    assert(positions.size() >= 3*count);
+    assert(distances.size() >= count);
+
+    unsigned batch_size = 512;
+    unsigned batches = (count+batch_size-1)/batch_size;
+    unsigned num_threads = MIN(sdfs.size(), batches/10 + 1); //do not spawn too many tiny threads
+
+    unsigned batches_per_thread = (batches+num_threads-1)/num_threads;
+
+    if (num_threads == 1)
+      evaluate_job(sdfs[0], positions, distances, batch_size, 0, count);
+    else
+    {
+      std::vector<std::thread> threads;
+      for (int i = 0; i < num_threads; i++)
+      {
+        unsigned offset = batch_size*batches_per_thread*i;
+        unsigned end = (i==num_threads-1) ? count : batch_size*batches_per_thread*(i+1);
+        threads.push_back(std::thread(&evaluate_job, sdfs[i], positions, distances, batch_size, offset, end));
+      }
+
+      for (auto &t : threads)
+        t.join();
+    }
+  }
+
+  void render_sdf_to_array_batched(std::span<float> out_array, const ProceduralSdf &sdf, const CameraSettings &camera, int image_w, int image_h, 
+                                   int spp, SDFRenderMode mode)
+  {
+    constexpr unsigned max_threads = 16;
+    std::vector<ProceduralSdf> sdfs;
+    for (int i=0;i<max_threads;i++)
+      sdfs.emplace_back(sdf);
+
+    AABB bbox = sdf.get_bbox();
+    AABB inflated_bbox = bbox.expand(1.1f);
+
+    glm::mat4 projInv = glm::inverse(camera.get_proj());
+    glm::mat4 viewInv = glm::inverse(camera.get_view());
+    glm::vec3 light_dir = normalize(camera.origin + glm::vec3(camera.origin.z, camera.origin.y, camera.origin.x) - camera.target);
+
+    int spp_a = MAX(1,floor(sqrtf(spp)));
+    float mult = 1.0f/(spp_a*spp_a);
+
+    std::vector<int> hits(image_w*image_h, 0); //one per pixel
+    std::vector<int> pixel_indices(image_w*image_h*spp_a*spp_a);
+    std::vector<glm::vec3> dirs(image_w*image_h*spp_a*spp_a);
+    std::vector<float> points(3*image_w*image_h*spp_a*spp_a);
+    std::vector<float> distances(image_w*image_h*spp_a*spp_a, 0);
+    std::vector<float> fd_points, fd_distances, fd_pixel_indices;
+    if (mode == SDFRenderMode::LAMBERT)
+    {
+      fd_points.resize(6*3*image_w*image_h*spp_a*spp_a);
+      fd_distances.resize(6*image_w*image_h*spp_a*spp_a);
+      fd_pixel_indices.resize(image_w*image_h*spp_a*spp_a);
+    }
+    
+    int index = 0;
     for (int yi=0;yi<image_h;yi++)
     {
-      std::vector<float> cur_grad;
-      std::vector<float> ddist_dpos = {0,0,0};
       for (int xi=0;xi<image_w;xi++)
       {
-        float color = 0;
         for (int yp=0;yp<spp_a;yp++)
         {
           for (int xp=0;xp<spp_a;xp++)
@@ -112,44 +173,123 @@ namespace upg
             float y = (float)(yi*spp_a+yp)/(image_h*spp_a);
             float x = (float)(xi*spp_a+xp)/(image_w*spp_a);
             glm::vec3 dir = transformRay(EyeRayDirNormalized(x,y,projInv), viewInv);
-            glm::vec3 p0;
-            
-            if (sdf_sphere_tracing(sdf, sdf_bbox, camera.origin, dir, &p0))
+            glm::vec3 p0 = camera.origin;
+            float t = 0;
+            if (bbox.contains(p0) || bbox.intersects(p0, dir, &t))
             {
-              if (mode == SDFRenderMode::MASK)
-                color += 1;
-              else if (mode == SDFRenderMode::LAMBERT)
-              {
-                constexpr float h = 0.001;
-                cur_grad.clear();
-                float ddx = (sdf.get_distance(p0 + glm::vec3(h,0,0)) - sdf.get_distance(p0 + glm::vec3(-h,0,0)))/(2*h);
-                float ddy = (sdf.get_distance(p0 + glm::vec3(0,h,0)) - sdf.get_distance(p0 + glm::vec3(0,-h,0)))/(2*h);
-                float ddz = (sdf.get_distance(p0 + glm::vec3(0,0,h)) - sdf.get_distance(p0 + glm::vec3(0,0,-h)))/(2*h);
-                glm::vec3 n = glm::normalize(glm::vec3(ddx, ddy, ddz));
-                color += MAX(0.1f, dot(n, light_dir));
-              }
-              else if (mode == SDFRenderMode::DEPTH)
-              {
-                float z = glm::length(p0 - camera.origin);
-                float d = (1/z - 1/camera.z_near)/(1/camera.z_far - 1/camera.z_near);
-                color += d;
-              }
-              else if (mode == SDFRenderMode::LINEAR_DEPTH)
-              {
-                float z = glm::length(p0 - camera.origin);
-                color += (z - camera.z_near)/(camera.z_far - camera.z_near);
-              }
-              else if (mode == SDFRenderMode::INVERSE_LINEAR_DEPTH)
-              {
-                float z = glm::length(p0 - camera.origin);
-                color += 1 - (z - camera.z_near)/(camera.z_far - camera.z_near);
-              }
+              p0 += t*dir;
+              pixel_indices[index] = yi*image_w+xi;
+              dirs[index] = dir;
+              points[3*index + 0] = p0.x;
+              points[3*index + 1] = p0.y;
+              points[3*index + 2] = p0.z;
+              index++;
             }
+            //else ray won't intersect SDF
           }
         }
-        out_array[yi*image_w+xi] = color/SQR(spp_a);
       }
     }
+
+    constexpr int max_steps = 1000;
+    constexpr float EPS = 3e-6;
+    constexpr float h = 0.001;
+
+    int points_left = index;
+    int points_found = 0;
+    int iteration = 0;
+    while (iteration < max_steps && points_left > 0)
+    {
+      evaluate(sdfs, points, distances, points_left);
+
+      int new_points_left = points_left;
+      int free_pos = 0;
+      for (int i=0;i<points_left;i++)
+      {
+        float d = distances[i];
+        glm::vec3 p0 = glm::vec3(points[3*i+0], points[3*i+1], points[3*i+2]);
+        if (!inflated_bbox.contains(p0))//ray went too far, no hit
+        {
+          distances[i] = 2e6;
+          new_points_left--;
+        }
+        else if (d < EPS) //found hit!
+        {
+          distances[i] = 2e6;
+          new_points_left--;
+            if (mode == SDFRenderMode::LAMBERT)
+            {
+              std::vector<float> np = {points[3*i+0]+h, points[3*i+1]  , points[3*i+2],
+                                       points[3*i+0]-h, points[3*i+1]  , points[3*i+2],
+                                       points[3*i+0]  , points[3*i+1]+h, points[3*i+2],
+                                       points[3*i+0]  , points[3*i+1]-h, points[3*i+2],
+                                       points[3*i+0]  , points[3*i+1]  , points[3*i+2]+h,
+                                       points[3*i+0]  , points[3*i+1]  , points[3*i+2]-h};
+              for (int k=0;k<3*6;k++)
+                fd_points[3*6*points_found + k] = np[k];
+              fd_pixel_indices[points_found] = pixel_indices[i];
+            }
+            else if (mode == SDFRenderMode::MASK)
+            {
+              out_array[pixel_indices[i]] += mult;
+            }
+            else if (mode == SDFRenderMode::DEPTH)
+            {
+              float z = glm::length(p0 - camera.origin);
+              float d = (1 / z - 1 / camera.z_near) / (1 / camera.z_far - 1 / camera.z_near);
+              out_array[pixel_indices[i]] += d;
+            }
+            else if (mode == SDFRenderMode::LINEAR_DEPTH)
+            {
+              float z = glm::length(p0 - camera.origin);
+              out_array[pixel_indices[i]] += (z - camera.z_near) / (camera.z_far - camera.z_near);
+            }
+            else if (mode == SDFRenderMode::INVERSE_LINEAR_DEPTH)
+            {
+              float z = glm::length(p0 - camera.origin);
+              out_array[pixel_indices[i]] += 1 - (z - camera.z_near) / (camera.z_far - camera.z_near);
+            }
+            points_found++;
+        }
+        else //ray marching should continue
+        {
+          while (free_pos < i && distances[free_pos] < 1e6)
+            free_pos++;
+          
+          points[3*free_pos + 0] = points[3*i + 0] + d*dirs[i].x;
+          points[3*free_pos + 1] = points[3*i + 1] + d*dirs[i].y;
+          points[3*free_pos + 2] = points[3*i + 2] + d*dirs[i].z;
+          if (free_pos != i)
+          {
+            pixel_indices[free_pos] = pixel_indices[i];
+            dirs[free_pos] = dirs[i];
+            distances[free_pos] = distances[i];
+            distances[i] = 2e6;
+          }
+        }
+      }
+      iteration++;
+      points_left = new_points_left;
+      //logerr("iteration %d, points left %d/%d", iteration, points_left, index);
+    }
+
+    if (mode == SDFRenderMode::LAMBERT)
+    {
+      evaluate(sdfs, fd_points, fd_distances, 6*points_found);
+      for (int i=0;i<points_found;i++)
+      {
+        float ddx = (fd_distances[6*i+0] - fd_distances[6*i+1])/(2*h);
+        float ddy = (fd_distances[6*i+2] - fd_distances[6*i+3])/(2*h);
+        float ddz = (fd_distances[6*i+4] - fd_distances[6*i+5])/(2*h);
+        glm::vec3 n = glm::normalize(glm::vec3(ddx, ddy, ddz));
+        out_array[fd_pixel_indices[i]] += mult*MAX(0.1f, dot(n, light_dir));
+      }
+    }
+  }
+
+  void render_sdf_to_array(std::span<float> out_array, const ProceduralSdf &sdf, const CameraSettings &camera, int image_w, int image_h, int spp, SDFRenderMode mode)
+  {
+    render_sdf_to_array_batched(out_array, sdf, camera, image_w, image_h, spp, mode); 
   }
 
   void sdf_to_outer_point_cloud(const ProceduralSdf &sdf, int points_count, std::vector<glm::vec3> *points, 
