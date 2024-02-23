867dce7729b076e43d12f7fbf4312102d3a9bee4
Sammael <albgar-14@yandex.ru>
2024-02-23 18:20:11 +0300

    neuralCore: removed hardcoded path from my local computer in slicer_execute.sh

--- modules/neuralCore/slicer_execute.sh
+++ modules/neuralCore/slicer_execute.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /home/sammael/kernel_slicer
-/home/sammael/kernel_slicer/kslicer /home/sammael/grade/modules/neuralCore/tensor_processor_impl.cpp -mainClass TensorProcessorImpl -stdlibfolder /home/sammael/kernel_slicer/TINYSTL -pattern ipv -reorderLoops YX -I/home/sammael/kernel_slicer/apps/LiteMath ignore -I/home/sammael/kernel_slicer/apps/LiteMathAux ignore -I/home/sammael/kernel_slicer/TINYSTL ignore -shaderCC glsl -suffix _GPU -DKERNEL_SLICER -v 
-cd /home/sammael/grade/modules/neuralCore
+cd ../../../kernel_slicer
+./kslicer ../grade/modules/neuralCore/tensor_processor_impl.cpp -mainClass TensorProcessorImpl -stdlibfolder $PWD/TINYSTL -pattern ipv -reorderLoops YX -I$PWD/apps/LiteMath ignore -I$PWD/apps/LiteMathAux ignore -I$PWD/TINYSTL ignore -shaderCC glsl -suffix _GPU -DKERNEL_SLICER -v 
+cd ../grade/modules/neuralCore
 rm -r ../../shaders_gpu
 mkdir ../../shaders_gpu
 cd shaders_gpu
