529a1748c9a10159f90cbb32f89a0aa43c66f01e
Sammael <albgar-14@yandex.ru>
2024-03-12 23:10:05 +0300

    sdf octree stub

---
+++ src/UPG_reconstruction/sdf_octree.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "upg.h"
+#include "generation_common.h"
+#include "common_utils/bbox.h"
+#include "sdf_node.h"
+#include <memory>
+#include <functional>
+
+namespace upg
+{
+  class OctreeSdfNode : public PrimitiveSdfNode
+  {
+  public:
+    OctreeSdfNode(const SdfNodeType::Type &_type, unsigned _nodes_limit) : PrimitiveSdfNode(_type)
+    { 
+      nodes_limit = _nodes_limit;
+    }
+    virtual ~OctreeSdfNode() = default;
+
+    virtual void get_distance_batch(unsigned     batch_size,
+                                    float *const positions,    
+                                    float *      distances,
+                                    float *      ddist_dparams,
+                                    float *      ddist_dpos,
+                            std::vector<float> & stack,
+                                    unsigned     stack_head) const override;
+
+    virtual unsigned param_cnt() const override { return nodes_count; } 
+    virtual std::vector<ParametersDescription::Param> get_parameters_block(AABB scene_bbox) const override;
+    //grid SDF is always set in a unit cube and transformed by other nodes if needed
+    virtual AABB get_bbox() const override { return AABB({-1,-1,-1},{1,1,1}); }
+    void set_voxel(const float3 &pos, float distance);
+    void set_voxel(const uint3 &vox, float distance);
+    void construct(std::function<float(const float3 &)> sdf);
+  protected:
+    float sample_bilinear(const float3 &pos, std::vector<float> *ddist_dp = nullptr, 
+                          std::vector<float> *ddist_dpos = nullptr) const;
+
+    unsigned nodes_limit;
+    unsigned nodes_count = 0;
+    float3 bbox_size = float3(2,2,2);
+  };
+}
